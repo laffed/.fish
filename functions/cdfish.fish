c8e43d9848707a8f17b8af64a861153ca93303a4
laffed <roarklaffed@gmail.com>
2023-04-28 10:46:50 -0500

    rm deprecated fns

--- functions/cdfish.fish
+++
@@ -1,3 +0,0 @@
-function cdfish  
-  cd $HOME/.config/fish/functions/
-end
--- functions/overmind.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function overmind --wraps='npx overmind-devtools@latest' --description 'alias overmind=npx overmind-devtools@latest'
-  npx overmind-devtools@latest $argv; 
-end
