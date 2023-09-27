fc3b9d01d9707bc77833ea0c0e765ba6c5929468
laffed <roarklaffed@gmail.com>
2023-09-27 11:40:32 -0500

    .

--- functions/sshcred.fish
+++ functions/sshcred.fish
@@ -1,3 +1,3 @@
  function sshcred --description 'prompts identity password for id_rsa to save credentials for current mac session'
-  command ssh-add ~/.ssh/id_rsa
+  command ssh-add ~/.ssh/id_ed25519_laffed
 end
---
+++ functions/tsrepl.fish
@@ -0,0 +1,5 @@
+function tsrepl --description 'start bun repl'
+  cd ~/Development/tsrepl
+  
+  command bun --watch index.ts
+end
